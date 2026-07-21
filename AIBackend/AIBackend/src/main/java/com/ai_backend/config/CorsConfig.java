a9dddf52ecfc597c15d9859b4939dbe2819c5361
KamineeChouhan29 <kamineechouhan75@gmail.com>
2026-07-21 21:55:53 +0530

    fixed vercel problem

--- AIBackend/AIBackend/src/main/java/com/ai_backend/config/CorsConfig.java
+++ AIBackend/AIBackend/src/main/java/com/ai_backend/config/CorsConfig.java
@@ -1,19 +1,34 @@
 package com.ai_backend.config;
 
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+    @Bean
+    public CorsFilter corsFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        //config.addAllowedOrigin("https://ai-chatbot-six-xi-40.vercel.app");
+
+        config.addAllowedOriginPattern("https://*.vercel.app");
+        config.addAllowedOrigin("http://localhost:4200");
+
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
     }
 }
